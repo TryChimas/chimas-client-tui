2a49d5251e29ac6a75d813daffddf1fc2c7fd046
kassius iacchus <kassivs@gmail.com>
2016-06-08 20:23:44 -0300

    changes being made

--- app.py
+++ app.py
@@ -18,6 +18,27 @@ palette = [
     ('selected', 'black', 'dark green'),
 ]
 
+class ListButton(urwid.Button):
+    def __init__(self,label,on_press=None,user_data=None):
+        super(ListButton, self).__init__("")
+        self._w = urwid.AttrMap(urwid.SelectableIcon([u"\N{BULLET} ", label]), 'unselected', 'selected')
+
+class BoardList(urwid.ListBox):
+    def __init__(self):
+        super(BoardList, self).__init__("")
+        self.load()
+
+    def load(self):
+
+        r = requests.get(host+'/boards', auth=authdata)
+        obj = json.loads(r.text)
+
+        mylist = [urwid.Divider()]
+        for c in obj['_items']:
+            mylist.append(ListButton('Board {0}'.format(c['title'])))
+        mylist.append(urwid.Divider())
+        self.body = urwid.SimpleFocusListWalker(mylist)
+
 class WindowFrame(urwid.Frame):
     def __init__(self, body=None, header=None, footer=None, focus_part='body'):
         super(WindowFrame, self).__init__("")
@@ -28,27 +49,13 @@ class WindowFrame(urwid.Frame):
         self.f_footer_text = urwid.Text('This is footer')
         self.footer = urwid.Padding(self.f_footer_text,left=2,right=2)
 
-        self.body = urwid.Padding(body, left=2, right=2)
+        self.set_body(BoardList())
 
-class ListButton(urwid.Button):
-    def __init__(self,label,on_press=None,user_data=None):
-        super(ListButton, self).__init__("")
-        self._w = urwid.AttrMap(urwid.SelectableIcon([u"\N{BULLET} ", label]), 'unselected', 'selected')
+    def set_body(self, widget):
+        self.body = urwid.Padding(widget, left=2, right=2)
 
-class BoardList(urwid.ListBox):
-    def __init__(self, body=None):
-        super(BoardList, self).__init__("")
 
-        r = requests.get(host+'/boards', auth=authdata)
-        obj = json.loads(r.text)
 
-        mylist = [urwid.Divider()]
-        for c in obj['_items']:
-            mylist.append(ListButton('Board {0}'.format(c['title'])))
-        mylist.append(urwid.Divider())
-        #f_list = urwid.ListBox(urwid.SimpleFocusListWalker(mylist))
-        #self.body = urwid.ListBox(urwid.SimpleFocusListWalker(mylist))
-        self.body = urwid.SimpleFocusListWalker(mylist)
 
 #content_filler = urwid.Filler(content_padding, top=3, bottom=3)
 l_boards = BoardList()
@@ -58,7 +65,7 @@ l_boards = BoardList()
 #w_main = urwid.Frame(content_padding, f_header, f_footer)
 
 #mylist=urwid.SimpleFocusListWalker(['lolz','okay'])
-w_main = WindowFrame(body=l_boards)
+w_main = WindowFrame()
 
 loop=urwid.MainLoop(w_main, palette)
 
