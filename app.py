19f5f324934037ae1f880e3ec0067c7229b19e05
kassius iacchus <kassivs@gmail.com>
2016-06-08 22:08:56 -0300

    threads beginning to work.

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ from requests.auth import HTTPBasicAuth
 host = "http://127.0.0.1:41345"
 authdata = ("admin", "abc123")
 
-PADDING_LEFT = 2
-PADDING_RIGHT = 2
-PADDING_TOP = 1
-PADDING_BOTTOM = 1
-
 palette = [
     (None, 'light gray', 'default'),
     ('unselected', 'white', 'default'),
@@ -43,15 +38,13 @@ class BoardList(urwid.ListBox):
         mylist.append(urwid.Divider())
         self.body = urwid.SimpleFocusListWalker(mylist)
 
-class BoardTopics(urwid.ListBox):
+class TopicList(urwid.ListBox):
     def __init__(self, main_window, user_data):
-        super(BoardTopics, self).__init__("")
+        super(TopicList, self).__init__("")
 
         self.main_window = main_window
         self.user_data = user_data
 
-        #import pprint
-        #pprint.pprint(button,width=1)
         self.load()
 
     def load(self):
@@ -61,7 +54,31 @@ class BoardTopics(urwid.ListBox):
 
         mylist = [urwid.Divider()]
         for c in obj['_items']:
-            mylist.append(ListButton('Topic {0}'.format(c['title']),self.main_window.boardlist_callback,c))
+            mylist.append(
+                ListButton('Topic {0}'.format(c['title']), self.main_window.topiclist_callback, c)
+            )
+        mylist.append(urwid.Divider())
+        self.body = urwid.SimpleFocusListWalker(mylist)
+
+class ThreadPosts(urwid.ListBox):
+    def __init__(self, main_window, user_data):
+        super(ThreadPosts, self).__init__("")
+
+        self.main_window = main_window
+        self.user_data = user_data
+
+        self.load()
+
+    def load(self):
+
+        r = requests.get(host+'/posts?where={"topic_id":"' + self.user_data['id'].__str__() + '"}', auth=authdata)
+        obj = json.loads(r.text)
+
+        mylist = [urwid.Divider()]
+        for c in obj['_items']:
+            mylist.append(
+                ListButton('Post: {0}'.format(c['title']), self.main_window.topiclist_callback, c)
+            )
         mylist.append(urwid.Divider())
         self.body = urwid.SimpleFocusListWalker(mylist)
 
@@ -83,22 +100,16 @@ class WindowFrame(urwid.Frame):
 
     def boardlist_callback(self, button, user_data):
 
-        self.boardtopics = BoardTopics(self, user_data)
+        self.topiclist = TopicList(self, user_data)
 
-        self.set_body(self.boardtopics)
+        self.set_body(self.topiclist)
 
+    def topiclist_callback(self, button, user_data):
 
+        self.threadposts = ThreadPosts(self, user_data)
 
+        self.set_body(self.threadposts)
 
-
-#content_filler = urwid.Filler(content_padding, top=3, bottom=3)
-#l_boards = BoardList()
-#content_padding = urwid.Padding(l_boards, left=2, right=2)
-
-#content= urwid.BoxAdapter(content_padding,40)
-#w_main = urwid.Frame(content_padding, f_header, f_footer)
-
-#mylist=urwid.SimpleFocusListWalker(['lolz','okay'])
 w_main = WindowFrame()
 
 loop=urwid.MainLoop(w_main, palette)
