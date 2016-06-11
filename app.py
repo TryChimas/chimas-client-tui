7582281329d570b43e8de89694587f80482cbff8
kassius iacchus <kassivs@gmail.com>
2016-06-11 12:41:57 -0300

    thread pile widget beginning to work

--- app.py
+++ app.py
@@ -60,27 +60,47 @@ class TopicList(urwid.ListBox):
         mylist.append(urwid.Divider())
         self.body = urwid.SimpleFocusListWalker(mylist)
 
-class ThreadPosts(urwid.ListBox):
-    def __init__(self, main_window, user_data):
-        super(ThreadPosts, self).__init__("")
+class ThreadPosts(urwid.Pile):
+    def __init__(self, main_window, user_data, widget_list=[], focus_item=None):
+
+        #super(ThreadPosts, self).__init__(widget_list)
+        super(ThreadPosts, self).__init__(widget_list)
 
         self.main_window = main_window
         self.user_data = user_data
 
         self.load()
 
+        #super(ThreadPosts, self).__init__(widget_list, focus_item)
+
+
     def load(self):
 
         r = requests.get(host+'/posts?where={"topic_id":"' + self.user_data['id'].__str__() + '"}', auth=authdata)
         obj = json.loads(r.text)
 
-        mylist = [urwid.Divider()]
+        #mylist = [urwid.Divider()]
+        mylist = []
         for c in obj['_items']:
-            mylist.append(
-                ListButton('Post: {0}'.format(c['title']), self.main_window.topiclist_callback, c)
+            #mylist.append(
+            mawids = urwid.Text(c['title'])
+            self.contents.append(
+                #('pack', urwid.Text(c['title']))
+                #(mawids, ('pack'))
+                (mawids, ('pack',None))
+                #urwid.Text(c['title'])
+
+                #ListButton('Post: {0}'.format(c['title']), self.main_window.topiclist_callback, c)
+                #urwid.LineBox( [
+                    #urwid.Text(c['title']),
+                    #urwid.Divider(),
+                    #urwid.Text(c['post_text'])#]
+                #])
             )
-        mylist.append(urwid.Divider())
-        self.body = urwid.SimpleFocusListWalker(mylist)
+        #mylist.append(urwid.Divider())
+        #self.body = urwid.SimpleFocusListWalker(mylist)
+        #self.widget_list = mylist
+        #self.contents.append(mylist)
 
 class WindowFrame(urwid.Frame):
     def __init__(self, body=None, header=None, footer=None, focus_part='body'):
@@ -106,9 +126,11 @@ class WindowFrame(urwid.Frame):
 
     def topiclist_callback(self, button, user_data):
 
-        self.threadposts = ThreadPosts(self, user_data)
+        self.threadposts = ThreadPosts(self, user_data=user_data)
 
         self.set_body(self.threadposts)
+        #myPile = urwid.Pile([('pack'), urwid.Text("oi"), urwid.Text("ei"), urwid.Text("hello buddy")])
+        #self.set_body(myPile)
 
 w_main = WindowFrame()
 loop = urwid.MainLoop(w_main, palette)
